be313a77f7a2dbe3101fa5d83af10c8c8e655e46
pluxery <84563687+pluxery@users.noreply.github.com>
2022-01-15 07:09:44 +1000

    fix queen

--- Factory/Factory.cpp
+++ Factory/Factory.cpp
@@ -9,9 +9,9 @@ IPieceModel *Factory::Create(const std::string &model, const int &y, const int &
         return checker;
 
     } else if (model == "Queen") {
-        //Создаем Subject (CheckerModel) с наблюдателем (QueenView : Observer)
-        auto *queen = new CheckerModel(y, x, Color(color));
-        const auto observer = new CheckerView();
+        //Создаем Subject (QueenModel) с наблюдателем (QueenView : Observer)
+        auto *queen = new QueenModel(y, x, Color(color));
+        const auto observer = new QueenView();
         queen->Attach(observer);
         return queen;
     }
